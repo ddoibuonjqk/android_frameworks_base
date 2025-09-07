8f767f60b0c30289ca1b7e43ee0820ce50409dc1
nhansp <nhan.094.vn@gmail.com>
2025-09-07 16:51:04 +0700

    Refactor to com.android.internal.util.jqk
    Signed-off-by: nhansp <nhan.094.vn@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/data/quickaffordance/AssistantKeyguardQuickAffordanceConfig.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/data/quickaffordance/AssistantKeyguardQuickAffordanceConfig.kt
@@ -31,7 +31,7 @@ import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 
-import com.android.internal.util.evolution.Utils
+import com.android.internal.util.jqk.Utils
 
 @SysUISingleton
 class AssistantKeyguardQuickAffordanceConfig
